84dfc7ee9c7dffa3205de6210fbcf722f07ec4e3
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2023-07-13 18:09:27 +0200

    Fixed table report method for penalties and numbers to work with multiple predictions

--- src/table_report/table_report.py
+++ src/table_report/table_report.py
@@ -4,30 +4,61 @@ import mediapipe as mp
 from src.hands_gesture_recognition.hand_gesture_recognition import hand_gesture_recognition, portable_model, model_type
 
 class table_report_obj:
-    number: any
-    foul: any
-    penalty: any
+    number: str
+    foul: str
+    penalty: str
+    predictions: dict
 
     def __init__(self) -> None:
         self.number = None
         self.foul = None
         self.penalty = None
+        self.predictions = {}
+
     
-def table_report(frame, hands, mp_hands, mp_drawing, mp_drawing_styles, table_rep: table_report_obj, cont) -> table_report_obj:
+def table_report(frame, hands, mp_hands, mp_drawing, mp_drawing_styles, table_rep: table_report_obj, cont_frame) -> table_report_obj:
     pt_model: portable_model
+    if(cont_frame==0 or cont_frame==101):
+        table_rep.predictions.clear()
     if table_rep.number == None:
         pt_model = portable_model(model_type.NUMBERS)
         num_pred = hand_gesture_recognition(frame, hands, mp_hands, mp_drawing, mp_drawing_styles, pt_model)
-        if(num_pred == -1):
-            print("Model not found. Please try again")
-        elif(num_pred!=None):
-            if(num_pred!=table_rep.number):
-                table_rep.number = num_pred
+        if cont_frame < 100:            
+            if(num_pred == -1):
+                print("Model not found. Please try again")
+            elif(num_pred!=None):
+                keys = table_rep.predictions.keys()
+                if(num_pred in keys):
+                    val = table_rep.predictions[num_pred]
+                    table_rep.predictions[num_pred] = val+1
+                else:
+                    table_rep.predictions[num_pred] = 1
+        if(cont_frame == 100):
+            print("FINISH")
+            if len(table_rep.predictions) == 0:
+                return None
+            table_rep.number = max(table_rep.predictions, key= lambda x: table_rep.predictions[x])
+            
+            # TODO: se è dorso fai cose diverse per table_rep.number (salva in altra variabile nell'oggetto?)
     elif table_rep.foul == None:
-        pass
+        table_rep.foul = "Personal"
     elif table_rep.penalty == None:
         pt_model = portable_model(model_type.PENALTY)
-        hand_gesture_recognition(frame, hands, mp_hands, mp_drawing, mp_drawing_styles, pt_model)
+        penalty_pred = hand_gesture_recognition(frame, hands, mp_hands, mp_drawing, mp_drawing_styles, pt_model)
+        if cont_frame < 200:
+            if(penalty_pred == -1):
+                print("Model not found. Please try again")
+            elif(penalty_pred!=None):
+                keys = table_rep.predictions.keys
+                if(penalty_pred in keys()):
+                    val = table_rep.predictions[penalty_pred]
+                    table_rep.predictions[penalty_pred] = val+1
+                else:
+                    table_rep.predictions[penalty_pred] = 1
+        if(cont_frame == 200):
+            if len(table_rep.predictions) == 0:
+                return None
+            table_rep.penalty = max(table_rep.predictions, key= lambda x: table_rep.predictions[x])
     return table_rep
 
 if __name__ == "__main__":
@@ -39,32 +70,46 @@ if __name__ == "__main__":
 
     cap = cv2.VideoCapture(0)
     table_rep: table_report_obj = table_report_obj()
+    op = 1
     cont = 0
-    stop_prediction = 300
-    table_rep.foul = "Personal"
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             print("Oh no")
             break
         
-        if(cont<stop_prediction):
+        #print(cont)
+        if(table_rep.number == None or table_rep.penalty == None or table_rep.foul == None):
             table_rep = table_report(frame, hands, mp_hands, mp_drawing, mp_drawing_styles, table_rep, cont)
-        else:
-            cont = 0
+            cont = cont+1
 
-        print(table_rep.number)
-        print(table_rep.foul)
-        print(table_rep.penalty)
+        print(cont)
+
+        if(table_rep == None):
+            print("Element is still None after prediction")
+            break
         
-        cv2.putText(frame, table_rep.number, (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,0,255),3, cv2.LINE_AA)
+        if(table_rep.number != None):
+            print("NUMBER PREDICTED "+str(table_rep.number))
+            cv2.putText(frame, table_rep.number, (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,0,255),3, cv2.LINE_AA)
+        if(table_rep.foul != None):
+            print("NUMBER PREDICTED "+str(table_rep.foul))
+            cv2.putText(frame, table_rep.number, (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,0,255),3, cv2.LINE_AA)
+            cv2.putText(frame, table_rep.foul, (0,75), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,255,0),3, cv2.LINE_AA)
+        if(table_rep.penalty != None):
+            print("NUMBER PREDICTED "+str(table_rep.penalty))
+            cv2.putText(frame, table_rep.number, (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,0,255),3, cv2.LINE_AA)
+            cv2.putText(frame, table_rep.foul, (0,75), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,255,0),3, cv2.LINE_AA)
+            cv2.putText(frame, table_rep.penalty, (0,100), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255,0,0),3, cv2.LINE_AA)
+            
+
+        #cv2.putText(frame, table_rep.number, (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,0,255),3, cv2.LINE_AA)
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(25) == ord('q'):
             break
         if cv2.waitKey(25) == ord('r'):
             table_rep = table_report_obj()
-            table_rep.foul = "Personal"
 
     cap.release()
     cv2.destroyAllWindows()
