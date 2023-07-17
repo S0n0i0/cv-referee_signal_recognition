cc4ad7847117c8bc7040b1ae7d692fd5849df092
Simone <simone.compri@gmail.com>
2023-07-17 19:27:46 +0200

    Data augmentation

--- src/fouls_gesture_recognition/collect_input.py
+++ src/fouls_gesture_recognition/collect_input.py
@@ -1,15 +1,27 @@
 import os
-
 import cv2
 from src.commons.utils import mediapipe_detection,draw_styled_landmarks,extract_keypoints
 from src.commons.data_structures import PATH
 import numpy as np
 import mediapipe as mp
+import threading
 
 def get_resized_dims(img,scale_percent):
-    width = int(frame.shape[1] * scale_percent / 100)
-    height = int(frame.shape[0] * scale_percent / 100)
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
     return (width, height)
+
+def analyze_frame(frame,analyzed_frame,frame_num,holistic,mp_holistic,mp_drawing,category,action,file_name):
+    # Make detections
+    image, results = mediapipe_detection(frame, holistic)
+
+    # Draw landmarks
+    draw_styled_landmarks(image, results, mp_holistic, mp_drawing)
+    
+    # Save keypoints
+    keypoints = extract_keypoints(results)
+    npy_path = os.path.join(PATH.DATA, category, action, f"{file_name}_{analyzed_frame}", str(frame_num))
+    np.save(npy_path, keypoints)
 
 category = "fouls"
 sequence_length = 30
@@ -46,6 +58,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         # List of svo files contained in action
         files = [os.path.join(PATH.SAMPLES, category, action, file) for file in os.listdir(os.path.join(PATH.SAMPLES, category, action)) if file.endswith('.mp4')]
 
+        threads = []
         for file in files:
             file_name = file.split('\\')[-1].split('.')[0]
             for alternative in frame_alternatives:
@@ -65,6 +78,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
                     # Draw landmarks
                     draw_styled_landmarks(image, results, mp_holistic, mp_drawing)
+
                     frames_to_analyze = {
                         "original": frame,
                         "flipped": cv2.flip(frame,1),
@@ -73,21 +87,19 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                     }
                     
                     for analyzed_frame in frames_to_analyze.keys():
+                        thread = threading.Thread(target=analyze_frame,args=(frames_to_analyze[analyzed_frame],analyzed_frame,frame_num,holistic,mp_holistic,mp_drawing,category,action,file_name))
+                        thread.start()
+                        threads.append(thread)
 
-                        # Make detections
-                        image, results = mediapipe_detection(frames_to_analyze[analyzed_frame], holistic)
-
-                        # Draw landmarks
-                        draw_styled_landmarks(image, results, mp_holistic, mp_drawing)
-                        
-                        # Save keypoints
-                        keypoints = extract_keypoints(results)
-                        npy_path = os.path.join(PATH.DATA, category, action, f"{file_name}_{analyzed_frame}", str(frame_num))
-                        np.save(npy_path, keypoints)
                         cv2.waitKey(10)
+                    
+                    for thread in threads:
+                        thread.join()
                 else:
                     break
             
             cap.release()
             cv2.destroyAllWindows()
-            print("Finished")
+            print(f"Finished: {file}")
+    
+    print(f"{action} analyzed")
